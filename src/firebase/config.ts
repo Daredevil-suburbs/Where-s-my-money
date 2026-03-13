af660c428309dc5b643c6c564785ae1f2f9798cd
Deadline <deadlineshady@gmail.com>
2026-03-13 14:30:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4160640769-ada65",
+  "appId": "1:423719544935:web:48c31eef23c57ce523ccc2",
+  "apiKey": "AIzaSyC4jFTdxuNI-P2hBSKzaeh0gXt7e2KJmh0",
+  "authDomain": "studio-4160640769-ada65.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "423719544935"
+};
